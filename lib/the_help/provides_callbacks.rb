380d16f001f2d9c44c1bc9f659b404786c3394bf
John Wilger <john@johnwilger.com>
2018-10-28 19:59:25 -0700

    Allow regular methods to be turned into callback
    It basically just wraps the method in code that adds the logging
    behavior.

--- lib/the_help/provides_callbacks.rb
+++ lib/the_help/provides_callbacks.rb
@@ -9,11 +9,18 @@ module TheHelp
   #
   #     def do_something
   #       collaborator.do_some_other_thing(when_done: callback(:it_was_done))
+  #       collaborator
+  #         .do_some_other_thing(when_done: callback(:it_was_done_method))
   #     end
   #
   #     callback(:it_was_done) do |some_arg:|
   #       puts "Yay! #{some_arg}"
   #     end
+  #
+  #     def it_was_done_method(some_arg:)
+  #       puts "In a method: #{some_arg}"
+  #     end
+  #     callback :it_was_done_method
   #   end
   #
   #   class Bar
@@ -26,11 +33,7 @@ module TheHelp
   #   f.collaborator = Bar.new
   #   f.do_something
   #   # STDOUT: "Yay! done by Bar"
-  #
-  # Callbacks can be given to collaborating objects, but the actual methods are
-  # defined as private methods. This allows the object to control which other
-  # objects are able to invoke the callbacks (at least to the extent that Ruby
-  # lets you do so.)
+  #   # STDOUT: "In a method: done by Bar"
   #
   # If the including class defines a #logger instance method, a debug-level
   # message will be logged indicating that the callback was invoked.
@@ -45,28 +48,58 @@ module TheHelp
     # Classes that include ProvidesCallbacks are extended with these
     # ClassMethods
     module ClassMethods
+      private
+
       # Defines a callback method on the class
       #
-      # The provided block will be used to define an instance method. This
-      # behaves similarly to #define_method, however it will ensure that
-      # callbacks are logged if the object has a #logger method defined.
+      # Regardless of whether the callback is pointing to an existing instance
+      # method or if it is defined via the block argument, the callback will
+      # also be wrapped in logging statements that can help you trace the
+      # execution path through your code in the event of any anomolies.
       #
-      # @param name [Symbol] The name of the callback
-      # @param block [Proc] The code that will be executed in the context of the
-      #   object when the callback is invoked.
+      # @param name [Symbol] The name of the callback. If no block is provided,
+      #        then name must be the name of an existing instance method.
+      # @param block [Proc] If a block is provided, the block will act as the
+      #        though it is the body of an instance method when the callback is
+      #        invoked.
       # @return [self]
       def callback(name, &block)
-        define_method("#{name}_without_logging", &block)
+        without_logging = "#{name}_without_logging".to_sym
+        provides_callbacks_define_method_with_block(without_logging, &block)
+        provides_callbacks_alias_method(without_logging, name)
+        provides_callbacks_define_wrapper(name, without_logging)
+        self
+      end
+
+      def provides_callbacks_method_defined?(name)
+        method_defined?(name) || private_method_defined?(name)
+      end
+
+      def provides_callbacks_define_method_with_block(without_logging, &block)
+        return unless block_given?
+
+        define_method(without_logging, &block)
+        private without_logging
+      end
+
+      def provides_callbacks_alias_method(without_logging, name)
+        return unless provides_callbacks_method_defined?(name)
+
+        alias_method without_logging, name
+        private without_logging
+      end
+
+      def provides_callbacks_define_wrapper(name, without_logging)
+        make_public = public_method_defined?(name)
         define_method(name) do |*args|
           if defined?(logger)
             logger.debug("#{self.class.name}/#{__id__} received callback " \
                          ":#{name}.")
           end
-          send("#{name}_without_logging", *args)
+          send(without_logging, *args)
           self
         end
-        private name
-        self
+        private name unless make_public
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 
 require 'bundler/setup'
 require 'the_help'
+require 'byebug'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
